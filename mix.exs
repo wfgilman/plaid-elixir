04576394ea99f2f05d232e3f83b1c8f1849f2882
Will Gilman <will@samadhi.tech>
2016-10-04 21:39:17 -0700

    Updated version to reflect struct refactoring.

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Plaid.Mixfile do
 
   def project do
     [app: :plaid,
-     version: "0.1.0",
+     version: "0.2.0",
      description: @description,
      package: package(),
      elixir: "~> 1.3",
