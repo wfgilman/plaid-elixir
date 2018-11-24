25e5654b74a949403b095dbfc085ce82436185a0
Will Gilman <will@samadhi.tech>
2018-11-24 13:16:21 -0800

    Suppressed warning, updated ex_doc to fix runtime error.

--- mix.exs
+++ mix.exs
@@ -16,7 +16,7 @@ defmodule Plaid.Mixfile do
      docs: docs(),
      source_url: "https://github.com/wfgilman/plaid-elixir",
      test_coverage: [tool: ExCoveralls],
-     preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test]
+     preferred_cli_env: [coveralls: :test, "coveralls.detail": :test]
     ]
   end
 
@@ -34,7 +34,7 @@ defmodule Plaid.Mixfile do
       {:bypass, "~> 0.8", only: [:test]},
       {:credo, "~> 0.5", only: [:dev], runtime: false},
       {:excoveralls, "~> 0.6", only: [:test]},
-      {:ex_doc, "~> 0.16", only: [:dev], runtime: false}
+      {:ex_doc, "~> 0.19", only: [:dev], runtime: false}
     ]
   end
 
