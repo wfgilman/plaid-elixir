a6539ad14f1832a2dc5ea3cbc4c1e5554ae8ef2f
Stephen Bussey <sb8244@cs.ship.edu>
2022-08-08 11:51:02 -0400

    Sync transactions (#80)
    * Support transactions/sync API
    
    Very similar to transactions/get, but requires keeping a cursor in between calls and tracking changes
    to transactions over time.
    
    Transaction sync has replaced the use case for transaction get in most situations.
    
    * format
    
    * Fix up doc typo

--- test/lib/plaid/transactions_test.exs
+++ test/lib/plaid/transactions_test.exs
@@ -98,4 +98,69 @@ defmodule Plaid.TransactionsTest do
       assert {:error, %Plaid.Error{}} = Plaid.Transactions.get(params, config)
     end
   end
+
+  describe "transactions sync/2" do
+    @tag :unit
+    test "submits request and unmarshalls response", %{params: params, config: config} do
+      PlaidMock
+      |> expect(:send_request, fn request, _client ->
+        assert request.method == :post
+        assert request.endpoint == "transactions/sync"
+        assert %{metadata: _} = request.opts
+        {:ok, %Tesla.Env{}}
+      end)
+      |> expect(:handle_response, fn _response, mapper ->
+        body = http_response_body(:transactions_sync)
+        {:ok, mapper.(body)}
+      end)
+
+      assert {:ok, ds} = Plaid.Transactions.sync(params, config)
+      assert Plaid.Transactions.Sync == ds.__struct__
+      assert Plaid.Transactions.Transaction == List.first(ds.added).__struct__
+      assert Plaid.Transactions.Transaction == List.first(ds.modified).__struct__
+      assert Plaid.Transactions.RemovedTransaction == List.first(ds.removed).__struct__
+    end
+
+    @tag :integration
+    test "success integration test", %{params: params} do
+      bypass = Bypass.open()
+
+      config = %{
+        client_id: "test_id",
+        secret: "test_secret",
+        root_uri: "http://localhost:#{bypass.port}/"
+      }
+
+      body = http_response_body(:transactions_sync)
+
+      Bypass.expect(bypass, fn conn ->
+        conn
+        |> Plug.Conn.put_resp_header("content-type", "application/json")
+        |> Plug.Conn.resp(200, Poison.encode!(body))
+      end)
+
+      assert {:ok, %Plaid.Transactions.Sync{}} = Plaid.Transactions.sync(params, config)
+    end
+
+    @tag :integration
+    test "error integration test", %{params: params} do
+      bypass = Bypass.open()
+
+      config = %{
+        client_id: "test_id",
+        secret: "test_secret",
+        root_uri: "http://localhost:#{bypass.port}/"
+      }
+
+      body = http_response_body(:error)
+
+      Bypass.expect(bypass, fn conn ->
+        conn
+        |> Plug.Conn.put_resp_header("content-type", "application/json")
+        |> Plug.Conn.resp(400, Poison.encode!(body))
+      end)
+
+      assert {:error, %Plaid.Error{}} = Plaid.Transactions.sync(params, config)
+    end
+  end
 end
