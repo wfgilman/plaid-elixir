0e0ae47c7cf0f534987c87ae9ca52708a534edba
Will Gilman <will@samadhi.tech>
2023-09-16 14:10:16 -0700

    Bump version

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Plaid.Mixfile do
   def project do
     [
       app: :plaid,
-      version: "3.1.0",
+      version: "3.2.0",
       elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       package: package(),
